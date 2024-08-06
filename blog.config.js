e7e9b9138163a46cfef2c7d1cae7f507c6f645c4
Git_ca <31660214+gitca233@users.noreply.github.com>
2024-08-07 00:48:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,12 +20,12 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '入门项目经理，欢迎交流',
+    '入门项目经理，OA学习中，欢迎交流',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ggca', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '记点笔记', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ggca', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '入门项目经理', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.4041024.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
